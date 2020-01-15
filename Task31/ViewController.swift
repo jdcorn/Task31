ac3abf101f14e18220f89397a15d3cddffd3eaaf
jon <jdcorn@live.com>
2020-01-15 13:50:53 -0700

    Initial Commit

---
+++ Task31/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task31
+//
+//  Created by Jon Corn on 1/15/20.
+//  Copyright © 2020 jdcorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
